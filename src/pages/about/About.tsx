ff9f0fb17871b6bc75d28a85c3039f3cbb47d1b2
Lea <33721141+charrx@users.noreply.github.com>
2023-04-27 23:25:49 +0200

    feat: about page v1 (#6)

---
+++ src/pages/about/About.tsx
@@ -0,0 +1,11 @@
+import styles from "./About.module.scss";
+
+const About = () => {
+  return (
+    <section id="about" className={styles.about_container}>
+      <h2>Hello</h2>
+    </section>
+  );
+};
+
+export default About;
