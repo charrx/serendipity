4c3c6db1bd81d0ace08b365504dbf7f781b9f2c7
charrx <leanygaard97@gmail.com>
2023-04-27 23:32:58 +0200

    feat: more styling and content

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
+import NavBar from "../../components/navbar/Navbar";
+import { useSmoothScrollOnClick } from "../../utils/useSmoothScrollOnClick";
+import styles from "./Home.module.scss";
+
 const Home = () => {
+  const [showCursor, setShowCursor] = useState(true);
+
+  // Sets the interval for the cursor to blink
+  setInterval(() => {
+    setShowCursor(!showCursor);
+  }, 1000);
+
   return (
-    <div>
-      <p>Under Construction</p>
-    </div>
+    <>
+      <NavBar />
+      <section id="home_page" className={styles.home_container}>
+        <span className={styles.jobTitle}>Software Engineer</span>
+        <span className={styles.devName}>
+          Charrx
+          <span className={showCursor ? styles.showCursor : styles.hideCursor}>
+            |
+          </span>
+        </span>
+        <a href="#about" className={styles.navigation_container} onClick={useSmoothScrollOnClick}>
+          Explore
+          <div className={styles.navigation_item}></div>
+        </a>
+      </section>
+    </>
   );
 };
 
