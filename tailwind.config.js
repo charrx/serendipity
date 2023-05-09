f4ce1a801b2e21a5e7da59b52e9910d941ced280
charrx <leanygaard97@gmail.com>
2023-05-10 00:41:51 +0200

    chore: changes values of letterspacing to rem instead of em

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    letterSpacing: {
+      tightest: "-.075rem",
+      tighter: "-.05rem",
+      tight: "-.025rem",
+      normal: "0",
+      wide: ".025rem",
+      wider: ".05rem",
+      widest: ".1rem",
+    },
   },
   plugins: [],
 };
